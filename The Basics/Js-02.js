613917b2ac0fd22e11ea4bef34166aa2f2c43da1
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-03-14 16:53:37 +0500

    Js-02

---
+++ The Basics/Js-02.js
@@ -0,0 +1,15 @@
+// 1: Starts With X
+function startsWithX(string) {
+    if(string[0].toLowerCase() === "x") {
+        return true;
+    }
+    return false;
+}
+
+// 2: Starts With Casing
+function startsWithX(string) {
+    if(string[0].toLowerCase() === "x") {
+        return true;
+    }
+    return false;
+}
