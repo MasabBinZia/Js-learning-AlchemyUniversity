81924da96873c0d6404e2404765a5bc6b4dba538
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-03-31 15:24:33 +0500

    12

--- The Basics/Exercises/paractise-02.js
+++ The Basics/Exercises/paractise-02.js
@@ -29,3 +29,41 @@ function countC(str) {
 
     return count;
 }
+
+// 1: Count Vowels
+const vowels = ["a", "e", "i", "o", "u"];
+
+function countVowels(str) {
+    let count = 0;
+    for(let i = 0; i < str.length; i++) {
+        if(vowels.includes(str[i].toLowerCase())) count++;
+    }
+
+    return count;
+}
+
+//1: Reverse
+function reverse(string) {
+    let newStr = "";
+
+    for(let i = string.length - 1; i >= 0; i--) {
+        newStr += string[i]
+    }
+
+    return newStr;
+}
+
+//1: Palindrome
+function isPalindrome(string) {
+    return string === reverse(string);
+}
+
+function reverse(string) {
+    let newStr = "";
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        newStr += string[i]
+    }
+
+    return newStr;
+}
