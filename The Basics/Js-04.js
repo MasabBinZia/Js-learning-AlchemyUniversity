4347f04ea1e14c2aacdb880366c354ef56f559d5
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-04-10 17:29:28 +0500

    17

--- The Basics/Js-04.js
+++ The Basics/Js-04.js
@@ -38,3 +38,100 @@ function fetchAge(name, cb) {
     cb(celebrities[name]);
 }
 
+//1: Taking Shape
+function Shape(x, y) {
+    this.position = { x, y }
+}
+
+// 2: Move Shape
+function Shape(x, y) {
+    this.position = { x, y }
+}
+
+Shape.prototype.move = function(x,y) {
+    this.position.x += x;
+    this.position.y += y;
+}
+
+// 3: Circle Shape
+const Shape = require('./Shape');
+
+function Circle(x, y, radius) {
+    Shape.call(this, /* pass arguments to shape */);
+    // store radius on this
+}
+function Shape(x, y) {
+    this.position = { x, y }
+}
+
+Shape.prototype.move = function (x, y) {
+    this.position.x += x;
+    this.position.y += y;
+}
+
+// 4: Move Circle
+const Shape = require('./Shape');
+
+function Circle(x, y, radius) {
+    Shape.call(this, x, y);
+    this.radius = radius;
+}
+
+Circle.prototype = Object.create(Shape.prototype);
+
+function Shape(x, y) {
+    this.position = { x, y }
+}
+
+Shape.prototype.move = function (x, y) {
+    this.position.x += x;
+    this.position.y += y;
+}
+
+//5: Rectangle
+const Shape = require('./Shape');
+
+function Rectangle(x, y, height, width) {
+    Shape.call(this, x, y);
+    this.height = height;
+    this.width = width;
+}
+
+Rectangle.prototype = Object.create(Shape.prototype);
+
+function Shape(x, y) {
+    this.position = { x, y }
+}
+
+Shape.prototype.move = function (x, y) {
+    this.position.x += x;
+    this.position.y += y;
+}
+
+
+// 6: Rectangle Flip
+const Shape = require('./Shape');
+
+function Rectangle(x, y, height, width) {
+    Shape.call(this, x, y);
+    this.height = height;
+    this.width = width;
+}
+
+Rectangle.prototype = Object.create(Shape.prototype);
+
+Rectangle.prototype.flip = function() {
+    const width = this.width;
+    this.width = this.height;
+    this.height = width;
+}
+
+function Shape(x, y) {
+    this.position = { x, y }
+}
+
+Shape.prototype.move = function (x, y) {
+    this.position.x += x;
+    this.position.y += y;
+}
+
