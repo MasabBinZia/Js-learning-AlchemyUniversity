d136e8a3863a60f8ebd2a46e5c624c54cf60af75
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-04-15 19:50:32 +0500

    Ds-02

--- Data Structures/Js-01.js
+++ Data Structures/Js-01.js
@@ -9,3 +9,43 @@ function sortDown(array) {
         return b - a;
     });
 }
+
+// 3: Strings Ascending
+function sortStringsUp(array) {
+    array.sort((a,b) => {
+        return a.localeCompare(b);
+    });
+}
+
+// 4: Strings Descending
+function sortStringsDown(array) {
+    array.sort((a,b) => {
+        return b.localeCompare(a);
+    });
+}
+
+
+// 5: Sort Students
+function sortStudents(students) {
+    students.sort((a,b) => {
+        if (a.graduated && !b.graduated) {
+            return -1;
+        } 
+        if (b.graduated && !a.graduated) {
+            return 1;
+        }
+        return b.grade - a.grade;
+    });
+}
+
+// 6: Sort By Month
+const MONTHS = [
+    'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 
+    'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'
+];
+
+function sortByMonth(array) {
+    array.sort((a,b) => {
+        return MONTHS.indexOf(a.month) - MONTHS.indexOf(b.month);
+    });
+}
